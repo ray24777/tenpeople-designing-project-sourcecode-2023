e79421b6c4af519f36e71e1a2d595cb7e6b0cdb6
Gletscher233 <90560193+Gletscher233@users.noreply.github.com>
2023-05-19 20:28:46 +0800

    Update main.py, add arrow detection.

--- openmv/main.py
+++ openmv/main.py
@@ -1,12 +1,9 @@
-# Canny边缘检测:
-#
-# 这个例子展示了Canny边缘检测。
-import sensor, image, time
+import sensor, image, time, os, tf, math, uos, gc
 from pyb import UART,Timer
 import micropython
 
 # 串口
-uart = UART(3, 115200, timeout_char = 1000) # 实例化
+uart = UART(3, 115200, timeout_char = 1000)
 
 # 初始化sensor.
 sensor.reset()
@@ -23,7 +20,7 @@ sensor.set_gainceiling(8) # 设置相机图像增益上限
 clock = time.clock()
 
 # 设置使用模型
-model = 0
+model = 1
 
 #def a_func():
     #global command
@@ -93,21 +90,16 @@ def line_patrol():
 
             if w>=0:
                 if -10<w<10:
-                    command = 'a'+'1'+'0'+str(abs(w))
+                    command = "151000"+str(abs(w))
                 else:
-                    command = 'a'+'1'+str(abs(w))
+                    command = "15100"+str(abs(w))
             else:
                 if -10<w<10:
-                    command = 'a'+'0'+'0'+str(abs(w))
+                    command = "151010"+str(abs(w))
                 else:
-                    command = 'a'+'0'+str(abs(w))
+                    command = "15101"+str(abs(w))
 
-            # 发16进制数据
-            #code=[0x90,0x03,0x00,0x10,0x00,0x02,0xC5,0xCE]	# 要发送的数据列表
-            #uart.write(bytes(code))	# 发送该HEX数据
-
-            # 发字符串数据
-            # a 1 039: a起始 0代表左，1代表右 039代表角度
+            # 05 00 013: 0代表负\左，1代表正\右，第一位是符号位，发送三个数：x,y,w
             uart.write(command)
             print("command: "+command)
 
@@ -115,8 +107,71 @@ def line_patrol():
 
 ## 模型1：箭头检测
 def identification_arrow():
-    clock.tick() # 追踪两个snapshots()之间经过的毫秒数.
-    img = sensor.snapshot() # 拍一张照片并返回图像。
+    sensor.reset()                         # Reset and initialize the sensor.
+    sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565
+    sensor.set_framesize(sensor.B128X128)  # Set frame size to 128x128, whic is equal to model input
+    sensor.set_windowing((128, 128))       # Set 128x128 window.
+    sensor.skip_frames(time=2000)          # Let the camera adjust.
+
+
+    net = None
+    labels = None
+    min_confidence = 0.7    # 需要根据实际情况设置置信度
+    num_arrow = 20          # 检测到num_arrow次箭头后计算方向，20时根据识别情况，需要大约3-6s
+
+    try:
+        # load the model, alloc the model file on the heap if we have at least 64K free after loading
+        net = tf.load("trained.tflite", load_to_fb=uos.stat('trained.tflite')[6] > (gc.mem_free() - (64*1024)))
+    except Exception as e:
+        raise Exception('Failed to load "trained.tflite", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+
+    try:
+        labels = [line.rstrip('\n') for line in open("labels.txt")] # 包含了识别的种类和顺序
+    except Exception as e:
+        raise Exception('Failed to load "labels.txt", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+
+    colors = [ # 用3种颜色显示不同的箭头
+        (255,   0,   0),
+        (  0, 255,   0),
+        (255, 255,   0),
+        (  0,   0, 255),
+    ]
+
+    clock = time.clock() #设置计时器记录帧率
+    ARROW = [] #记录识别出的不同箭头
+    clock.tick() #开始计时
+    while(len(ARROW) <= num_arrow): # 识别到x次箭头后退出模型
+
+        img = sensor.snapshot()
+    # detect() returns all objects found in the image (splitted out per class already)
+    # we skip class index 0, as that is the background, and then draw circles of the center
+    # of our objects
+
+        for i, detection_list in enumerate(net.detect(img, thresholds=[(math.ceil(min_confidence * 255), 255)])):
+            if (i == 0): continue # background class
+            if (len(detection_list) == 0): continue # no detections for this class
+            else:
+                if (i == 1): ARROW.append(i)
+                elif (i == 2): ARROW.append(i)
+                elif (i == 3): ARROW.append(i)
+
+            #print("********** %s **********" % labels[i])
+            for d in detection_list:
+                [x, y, w, h] = d.rect()
+                center_x = math.floor(x + (w / 2))
+                center_y = math.floor(y + (h / 2))
+                #print('x %d\ty %d' % (center_x, center_y))
+                img.draw_circle((center_x, center_y, 12), color=colors[i], thickness=2)
+
+    print(clock.fps(), "fps", end="\n\n")
+
+    direct = max(ARROW, key=lambda v: ARROW.count(v))
+    print("direction is: ",direct)
+    if(direct == 1): pass # 向前走，具体时长和方向（防止被倒下立牌困住）需要实际测试
+    elif(direct == 2): pass # 向左前走
+    elif(direct == 3): pass # 向右前走
+
+
 
 
 #------------------------------------------------------------------------------------------#
@@ -127,6 +182,7 @@ while(True):
         line_patrol()
     if model == 1:
         identification_arrow()
+        # 注意，箭头检测函数只应该执行一次，这里的逻辑需要调整
 
 
     print("now fps: "+str(clock.fps())) # 注意:你的OpenMV摄像头的运行速度只有它的一半
